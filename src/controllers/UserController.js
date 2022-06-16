c5601f7972c29059ebf2d476e7cb5bd8232e0c46
Hermes <hermesemmanueldev@gmail.com>
2022-06-15 23:22:25 -0300

    feat: user controller finish

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,22 +1,85 @@
+
+const res = require("express/lib/response");
 const User = require("../models/User");
 
-const list = async (req, res) => {
+const list = async (request, response) => {
   const users = await User.find({});
-  res.json(users);
+  response.send(users);
 }
 
-const create = async (req, res) => {
+const find = async (request, response) => {
+  const { user } = request;
+  response.send(user);
+}
+
+const login = async (request, response) => {
+  const { email, password } = request.body;
   try {
-    const { body } = req;
+    const user = await User.findByCredentials({ email, password });
+    const token = await user.generateAuthToken();
+    response.send({ user, token });
+  } catch (error) {
+    console.log(error);
+    response.status(403).send(error);
+  }
+};
+
+const logout = async (request, response) => {
+  try {
+    const { user, token } = request;
+    user.tokens = user.tokens.filter(t => t.token !== token);
+    await user.save();
+    response.send()
+  } catch (error) {
+    response.status(401).send({ error: "Unauthorized" });
+  }
+}
+
+const logoutAll = async (request, response) => {
+  try {
+    const { user } = request;
+    user.tokens = [];
+    await user.save();
+    response.send()
+  } catch (error) {
+    response.status(401).send({ error: "Unauthorized" });
+  }
+}
+
+const create = async (request, response) => {
+  try {
+    const { body } = request;
     const user = new User(body);
     await user.save();
-    res.status(201).send(user);
-  } catch (err) {
-    res.status(400).send({ error: err.message });
+    response.status(201).send(user);
+  } catch (error) {
+    response.status(400).send({ error: error.message });
+  }
+
+}
+
+const remove = async (request, response) => {
+  try {
+    const { _id } = request.user;
+    await User.findByIdAndRemove(_id);
+    response.status(200).send();
+  } catch (error) {
+    response.status(500).send({ error: "Internal server error!" });
+  }
+}
+
+const edit = async (request, response) => {
+  try {
+    const { body } = request;
+    const { user } = request;
+    Object.assign(user, body);
+    await user.save();
+    response.status(200).send(user);
+  } catch (error) {
+    response.status(500).send({ error: error.message });
   }
 }
 
 module.exports = {
-  list,
-  create,
-};
+  list, find, create, remove, edit, login, logout, logoutAll
+}
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const {
   list,
+  find,
   create,
+  remove,
+  edit,
+  login,
+  logout,
+  logoutAll,
 } = require('../controllers/UserController');
-const { auth } = require('../middleware/users.middleware');
+const { isValidUserBody, auth } = require('../middleware/users.middleware');
 
 const users_routes = express.Router();
 
 users_routes.get('/', auth, list);
+users_routes.get('/me', auth, find);
 users_routes.post('/', create);
-
+users_routes.delete('/me', auth, remove)
+users_routes.put('/me', isValidUserBody, auth, edit)
+users_routes.post('/login', isValidUserBody, login)
+users_routes.post('/logout', auth, logout);
+users_routes.post('/logout_all', auth, logoutAll);
 module.exports = users_routes;
