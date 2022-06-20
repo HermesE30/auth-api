9124f826bc5c30c050be35ec8e378e5fb1e58f04
Hermes <hermesemmanueldev@gmail.com>
2022-06-20 17:52:54 -0300

    fix: port

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ require('./db/mongoose');
 
 const app = require('./app');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running and listen on port ${port}...`);
